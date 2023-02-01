f185866b7b4b41e449521f158040469906f8c8ff
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-01 10:55:01 -0800

    More C# programs

---
+++ Basic_C#_Programs/MathandComparisonOperators.cs/MathandComparisonOperators.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathandComparisonOperators.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 5 + 10;
+            Console.WriteLine("five plus ten = " + total);
+            Console.ReadLine();
+            int difference = 10 - 5;
+            Console.WriteLine("ten minus five = " + difference.ToString());
+            Console.ReadLine();
+            int product = 10 * 5;
+            Console.WriteLine(product);
+            Console.ReadLine();
+            double qoutient = 100.1 / 17.5;
+            Console.WriteLine(qoutient);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Basic_C#_Programs/input.cs/input.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace input.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Functions that take user input and performs different operations on it
+            Console.WriteLine("Input a number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((Int32)num * 50);
+            Console.ReadLine();
+            Console.WriteLine("Input a number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine((Int32)num2 + 25);
+            Console.ReadLine();
+            Console.WriteLine("Input a number");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine((Int32)num3 / 12.5);
+            Console.ReadLine();
+            Console.WriteLine("Input a number");
+            int num4 = int.Parse(Console.ReadLine());
+            bool answer = true;
+            Console.WriteLine((Int32)num4 > 50);
+            Console.ReadLine();
+            Console.WriteLine("Input a number");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine((Int32)num5 % 7);
+            Console.ReadLine();
+
+
+        }
+    }
+}
