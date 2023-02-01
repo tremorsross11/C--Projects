df7ae04018e0f5ab6b588cf0cf5332f11da464d1
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-01 11:29:32 -0800

    fixes

--- Basic_C#_Programs/techacademy.cs
+++ Basic_C#_Programs/techacademy.cs
@@ -18,14 +18,13 @@ namespace VariablesandDatatypes.cs
             string yourCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
             string yourNum = Console.ReadLine();
-            Console.WriteLine(yourName + yourCourse + yourNum);
+            Convert.ToInt32(yourNum);
+            Console.WriteLine(yourName + " " + yourCourse + " " + yourNum);
+            Console.ReadLine();
             Console.WriteLine("Do you need help with anything? Please answer true or false");
-            bool answer = true;
-            string answer1 = Convert.ToString(answer);
+            string answer = Console.ReadLine();
+            bool answer1 = Convert.ToBoolean(answer);
             Console.WriteLine(answer1);
-            bool answer2 = false;
-            string answer3 = Convert.ToString(answer2);
-            Console.WriteLine(answer3);
             Console.WriteLine("Were there any positive experiences you would like to share or feedback you would like to provide?");
             string exp = Console.ReadLine();
             Console.WriteLine("How many hours did you study?");
@@ -33,6 +32,7 @@ namespace VariablesandDatatypes.cs
             int hours = Convert.ToInt32(h);
             Console.ReadLine();
             Console.WriteLine("Thank you for your answers! An instructor will respond to this shortly.");
+            Console.ReadLine();
 
 
 
