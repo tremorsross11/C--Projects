86de3c7f3d28cfb5973e8ad728838a77a2dd025c
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-06 16:14:18 -0800

    Boolean Programs

---
+++ Basic_C#_Programs/CarInsuranceProgram/CarInsuranceProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (yes or no)");
+            string dui = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = (age > 15) && (dui.ToLower() != "yes") && (tickets <= 3);
+            Console.WriteLine("Applicant qualified for car insurance: " + qualified);
+            Console.ReadLine();
+        }
+
+    }
+}
