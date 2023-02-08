c8ea8fe756be62d4e79a75f53db5c7e6cc1c89d7
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-08 15:11:29 -0800

    Booleans

---
+++ Basic_C#_Programs/Loops/Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool guess = number == 12;
+
+            while (guess)
+            {
+
+
+
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("you guessed 62. try again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("you guessed 29. try again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You win!");
+                        guess = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/dowhile/dowhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool booleanComparisonValue = true;
+            while (booleanComparisonValue)
+            {
+                Console.WriteLine("True");
+                booleanComparisonValue = false;
+            }
+
+            // Boolean Comparison using a Do-While Statement 
+
+            bool booleanComparisonValue2 = true;
+            do
+            {
+                Console.WriteLine("True");
+                booleanComparisonValue2 = false;
+            }
+            while (booleanComparisonValue2);
+
+            Console.Read();
+        }
+    }
+}
