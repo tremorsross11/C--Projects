b42e8431aebd6f5500c5929e9801a085fa643e2a
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-08 10:44:08 -0800

    More programs

---
+++ Basic_C#_Programs/branching/branching/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace branching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentTemp = 70;
+            int roomTemp = 70;
+
+            if (currentTemp == roomTemp)
+            {
+                Console.WriteLine("It is room temp");
+
+            }
+            else if (currentTemp > roomTemp)
+            {
+                Console.WriteLine("It is warmer than room temp");
+            }
+            else
+            {
+                Console.WriteLine("It is not room temp");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/shippingprogram/shippingprogram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shippingprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = int.Parse(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int length = int.Parse(Console.ReadLine());
+
+            if (width + height + length > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return;
+            }
+
+            int quote = (width * height * length * weight) / 100;
+
+            Console.WriteLine("Your total quote is: ${0}", quote);
+            Console.ReadLine();
+        }
+    }
+}
