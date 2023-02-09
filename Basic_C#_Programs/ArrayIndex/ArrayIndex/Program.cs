2e831e49faa2c8137b9c56a07603cb5707950eef
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-08 17:41:58 -0800

    Arrays and Lists

---
+++ Basic_C#_Programs/ArrayIndex/ArrayIndex/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] ArrayOfStrings = { "String1", "String2", "String3" };
+            Console.WriteLine("Select an index of the Array (0, 1, or 2):");
+            int index = int.Parse(Console.ReadLine());
+
+            // Display the string at the index the user selected
+            Console.WriteLine($"You chose the string at index {index}: {ArrayOfStrings[index]}");
+
+            // Declare one-dimensional array of integers
+            int[] ArrayOfIntegers = { 1, 2, 3 };
+            Console.WriteLine("Select an index of the Array (0, 1, or 2):");
+            int index2 = int.Parse(Console.ReadLine());
+
+            // Display the integer at the index the user selected
+            Console.WriteLine($"You chose the integer at index {index2}: {ArrayOfIntegers[index2]}");
+
+            // create a list of strings 
+            List<string> ListOfStrings = new List<string>() { "String1", "String2", "String3" };
+            Console.WriteLine("Select an index of the List (0, 1, or 2):");
+            int index3 = int.Parse(Console.ReadLine());
+
+            // Display the content at the index the user selected
+            if (index3 >= 0 && index3 <= ListOfStrings.Count - 1)
+            {
+                Console.WriteLine($"You chose the content at index {index3}: {ListOfStrings[index3]}");
+            }
+            else
+            {
+                
+                Console.WriteLine("The index you chose does not exist.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ArraysandLists/ArraysandLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysandLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> intList = new List<string>();
+            intList.Add("Hello");
+            intList.Add("there");
+            intList.Add("user");
+            intList.Remove("there");
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+
+
+
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+            //int[] numArray2 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+            //numArray2[5] = 650;
+
+            //Console.WriteLine(numArray1[2]);
+            //Console.ReadLine();
+        }
+    }
+}
