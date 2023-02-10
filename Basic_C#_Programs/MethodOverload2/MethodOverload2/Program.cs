4c83a19a44a09ba5d58ecbfb9407603ff1f65cad
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-10 12:35:54 -0800

    methofs

---
+++ Basic_C#_Programs/MethodOverload2/MethodOverload2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOperation = new MathOperation();
+
+            Console.WriteLine("Please enter a number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number (optional):");
+            string number2String = Console.ReadLine();
+            int? number2 = null;
+            if (!string.IsNullOrEmpty(number2String))
+            {
+                number2 = Convert.ToInt32(number2String);
+            }
+
+            int result = mathOperation.MathOperationCalculator(number1, number2);
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MathOperation
+    {
+        public int MathOperationCalculator(int value1, int? value2 = null)
+        {
+            int result;
+            if (value2 == null)
+            {
+                result = value1 * value1;
+            }
+            else
+            {
+                result = value1 * value2.Value;
+            }
+            return result;
+        }
+    }
+}
+    
+
