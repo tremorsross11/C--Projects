361a436d1b57bfdc706b81ee2f4ad2044ebc89ed
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-10 11:56:42 -0800

    More Programs

---
+++ Basic_C#_Programs/NumberMethods/NumberMethods/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberMethods
+{
+    public class MathOperations
+    {
+        public static int Addition(int num)
+        {
+            return num + 10;
+        }
+
+        public static int Subtraction(int num)
+        {
+            return num - 5;
+        }
+
+        public static int Multiplication(int num)
+        {
+            return num * 2;
+        }
+    }
+}
+
---
+++ Basic_C#_Programs/NumberMethods/NumberMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The result of your math operations are:");
+            Console.WriteLine(MathOperations.Addition(num));
+            Console.WriteLine(MathOperations.Subtraction(num));
+            Console.WriteLine(MathOperations.Multiplication(num));
+            Console.ReadLine();
+        }
+        
+    }
+}
