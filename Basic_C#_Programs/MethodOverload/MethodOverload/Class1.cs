b62d0844889269c9f957c5ac022b8fd53ff36c56
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-10 12:18:52 -0800

    methods

---
+++ Basic_C#_Programs/MethodOverload/MethodOverload/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    public class MathOperation
+    {
+        public int MathOperationCalculator(int value)
+        {
+            int result = value * value;
+            return result;
+        }
+
+        public int MathOperationCalculator(double value)
+        {
+            int result = (int)(value / 2);
+            return result;
+        }
+
+        public int MathOperationCalculator(string value)
+        {
+            int number;
+            int.TryParse(value, out number);
+            int result = number * number * number;
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOperation = new MathOperation();
+
+            int intResult = mathOperation.MathOperationCalculator(2);
+            Console.WriteLine("Result of the Integer: " + intResult);
+
+            int decimalResult = mathOperation.MathOperationCalculator(2.5);
+            Console.WriteLine("Result of the Decimal: " + decimalResult);
+
+            int stringResult = mathOperation.MathOperationCalculator("3");
+            Console.WriteLine("Result of the String: " + stringResult);
+
+            Console.ReadLine();
+        }
+    }
+}
