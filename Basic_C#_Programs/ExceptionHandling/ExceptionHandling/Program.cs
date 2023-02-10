7c90c8ff87b49b268b186c1f4df8e5f887a8e685
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-10 10:28:20 -0800

    exceptions

---
+++ Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int NumberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a number");
+                int NumberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("dividing the two...");
+                int NumberThree = NumberOne / NumberTwo;
+                Console.WriteLine(NumberOne + " divided by " + NumberTwo + " equals " + NumberThree);
+                Console.ReadLine();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringsandIntegers/StringsandIntegers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsandIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+
+                List<int> list = new List<int>();
+                list.Add(0);
+                list.Add(1);
+                list.Add(2);
+                list.Add(3);
+                Console.WriteLine(list.Count);
+                Console.ReadLine();
+
+                Console.WriteLine("Input a number");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine(i + " divided by " + num + " equals " + i / num);
+                    Console.ReadLine();
+
+                }
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
