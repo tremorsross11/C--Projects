e3c2b8c0cd0e11d71b593c88708c01da760a081b
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-13 18:14:37 -0800

    h

---
+++ Basic_C#_Programs/instantiate/instantiate/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instantiate
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee first, Employee second)
+        {
+            return (first.Id == second.Id);
+        }
+
+        public static bool operator !=(Employee first, Employee second)
+        {
+            return (first.Id != second.Id);
+        }
+    }
+}
---
+++ Basic_C#_Programs/instantiate/instantiate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instantiate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" };
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("The Employees are equal.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The Employees are not equal.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
