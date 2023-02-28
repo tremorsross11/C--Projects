8a210f41f0d0adc2739d27b2ef143f4933650dd6
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-02-28 13:47:37 -0800

    c#

---
+++ Basic_C#_Programs/DatetimeWebsite/DatetimeWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+
+namespace DatetimeWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        
+
+       
+    }
+}
